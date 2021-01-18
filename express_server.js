f0296486143fba081be688d5d7d8107ddd4ced99
Louis Coreas <l.g.coreas@gmail.com>
2021-01-18 23:03:57 +0000

    new url generator started

--- express_server.js
+++ express_server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+function generateRandomString() {
+  let randomString =''
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+  for(let i = 0; i< 6; i++){
+     randomString += characters.charAt(Math.floor(Math.random()*characters.length))
+  }
+return randomString
+}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -25,9 +35,16 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
 let shorturl = req.params.shortURL
  const longurl = urlDatabase[shorturl] 
   const templateVars = { shortURL: shorturl, longURL: longurl };
   res.render("urls_show", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
